3709a312d82e65d092f880f93b1f88060f38fbff
Stilianos Tsampas <zeronebones@gmail.com>
2011-07-23 17:40:42 +0300

    small fix

--- various/socbook.sql
+++ various/socbook.sql
@@ -23,7 +23,7 @@ CREATE USER socbook IDENTIFIED BY socbook;
 DROP database `socbook`;
 CREATE database `socbook` DEFAULT charset = `utf8`;
 
-GRANT ALL ON socbook TO socbook;
+GRANT ALL ON socbook.* TO socbook;
 
 USE socbook;
 
