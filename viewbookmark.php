fbe379d1403a48ee35e85fd588969c048bb1e81f
Lefteris Aslanoglou <leftos@gmail.com>
2011-07-24 15:57:24 +0300

    Everything must be OOP now, Bookmarks, Comments, Tags, etc. Also increasing and decreasing a bookmark's rating works now.
    Signed-off-by: Lefteris Aslanoglou <leftos@gmail.com>

--- viewbookmark.php
+++ viewbookmark.php
@@ -11,9 +11,62 @@
 	$thisPage = __VIEWBOOKMARK;
 ?>
 
+<!-- viewbookmark.php required php preparation code -->
+<?php
+	if (!(isset($_GET['bid'])))
+	{
+		if (!(isset($_POST['bid'])))
+		{
+			die("You didn't use a valid BID.");
+		}
+		$bid = $_POST['bid'];
+	} 
+	else
+	{
+		$bid = $_GET['bid'];
+	}			
+	$bk = new bookmark($bid);
+	fetchBookmark($bk);
+	
+	if (isset($_POST['action']))
+	{
+		if ($_POST['action'] == 'rating')
+		{
+			if ($_POST['dod'] == 'plusone') { plusone($bid); }
+			else minusone($bid);
+		}
+	}
+?>
+
 <head>
 	<title><?php echo($title); ?></title>
 	<link rel="stylesheet" type="text/css" href="style.css" />
+	
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+	<script type="text/javascript">
+      jQuery(document).ready(function() {
+        $("#plusOneLink").click(function() {
+        	$.post("viewbookmark.php",
+			  { action: "rating", dod: "plusone", bid: "<?=$bid?>" },
+			  function(data){
+			    location.reload();
+			    //alert("Data Loaded: " + data);
+			  }
+			);
+        	return false; // <--- important, prevents the link's href (hash in this example) from executing.
+        });
+        $("#minusOneLink").click(function() {
+        	$.post("viewbookmark.php",
+			  { action: "rating", dod: "minusone", bid: "<?=$bid?>" },
+			  function(data){
+			    location.reload();
+			    //alert("Data Loaded: " + data);
+			  }
+			);
+        	return false; // <--- important, prevents the link's href (hash in this example) from executing.
+        });
+      });
+    </script>
 </head>
 
 <body>
@@ -33,25 +86,29 @@
 	
 	<div id="content">
 		
-		<?php
-			$bid = $_GET['bid'];
-			$bk = new bookmark($bid);
-			fetchBookmark($bk);
-			fetchComments($bk);
-		?>
-		
 		<h2><?php echo($bk->getTitle()); ?></h2>
 		<p><a href="http://<?php echo($bk->getUrl()); ?>"><?php echo($bk->getUrl()); ?></a></p>
 		<p><?=__DESCRIPTION?>: <br /><?=$bk->getDesc() ?></p>
 		<p>
-			<?=__RATING?>: <?=$bk->getRating() ?>&nbsp;&nbsp;&nbsp;&nbsp;<img src="images/plus-8.png" /> <img src="images/minus-8.png" />
+			<?=__RATING?>: <?=$bk->getRating() ?>&nbsp;&nbsp;&nbsp;&nbsp;<a href="#" id="plusOneLink"><img src="images/plus-8.png" /></a>&nbsp;<a href="#" id="minusOneLink"><img src="images/minus-8.png" /></a>
+		</p>
+		<p>
+			<?=__TAGS?>: <?php 
+			for ($i=0; $i<$bk->getTagCount(); $i++)
+			{
+				if ($i > 0) echo (', ');
+				$tag = $bk->getTag($i);
+				echo ($tag->getTagW());
+			}
+			?> 
 		</p>
 		<p><?=__DATECREATED?>: <?=$bk->getDateCreated() ?></p>
 		<p>&nbsp;</p>
-		<p><? echo(__COMMENTS.' ('.count($bk->comments).')'); ?></p>
+		<p><? echo(__COMMENTS.' ('.$bk->getCommentCount().')'); ?></p>
 		<?php
-			foreach ($bk->comments as $comment)
+			for ($i=0; $i< $bk->getCommentCount(); $i++)
 			{
+				$comment = $bk->getComment($i);
 				$commentdate = $comment->getDatePosted();
 				$commenttitle = $comment->getTitle();
 				$commentdesc = $comment->getDesc();
