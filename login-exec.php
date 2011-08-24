6b173ca35674788c61cf8014ea7f9ea7eeb3250e
Stilianos Tsampas <zeronebones@gmail.com>
2011-08-24 18:20:10 +0300

    small fixes. really really small

--- login-exec.php
+++ login-exec.php
@@ -1,91 +1,52 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+require_once ("deps/session.inc");
+require_once ("deps/presentation.inc");
+require_once ("deps/database.inc");
 
-<html>
+$form_secret = $_POST['form_secret'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-<!-- Page dependent settings such as settitle -->
-<?php 
-	require_once("deps/session.inc");
-	require_once("deps/presentation.inc");
-	require_once("deps/database.inc");
-	$title = settitle(__LOGIN);
-	$thisPage = __LOGIN;
-?>
-
-<head>
-	<!-- Global head attributes and scripts (JQuery, etc.) -->	
-	<?php require_once('templates/head.inc') ?>
-	
-	<!-- Page-specific head attributes -->
-</head>
-
-<body>
-	<!-- Below should remain as is on every page -->
-	<div id="title">
-		<?php require_once('templates/layout/title.php'); ?>
-	</div>
-	
-	<div id="language">
-		<?php require_once('templates/layout/language.php'); ?>
-	</div>
-	
-	<div id="navigation">
-		<?php require_once('templates/layout/navigation.php'); ?>
-	</div>
-	<!-- Above should remain as is on every page -->
-	
-	<div id="content">
-		<?php
+if(isset($_SESSION['FORM_SECRET']))
+{
+	if(strcasecmp($form_secret, $_SESSION['FORM_SECRET']) === 0)
+	{
+		if(!$username || !$password)
+		{
+			echo __NOTALLDETAILS . '<br />';
+			exit ;
+		}
 		
-			$form_secret = $_POST['form_secret'];
-			$username=$_POST['username'];
-			$password=$_POST['password'];
-			
-			if(isset($_SESSION['FORM_SECRET']))
-			{
-				if(strcasecmp($form_secret, $_SESSION['FORM_SECRET'])===0)
-				{
-					if( !$username || !$password )
-					{
-						echo __NOTALLDETAILS.'<br />';
-						exit;
-					}
-					else 
-					{
-						unset($_SESSION['FORM_SECRET']);				
-					
-						if (!validateUser( $username, $password))
-						{
-							echo (__LOGINSUCCESS);
-							header("Location: profile.php?uid=".$_SESSION['UID']);
-							exit;
-						}
-						else
-						{
-							echo (__LOGINGFAIL);
-							header("Location: login-form.php");
-							exit;
-						}
-					}
-				}
-				else
-				{
-					//Invalid secret key
-					echo "something you did is wrong, you are not supposed to even print this";
-				}
-			}
-			else
-			{
-				//Secret key missing
-				echo "form data has been processed";
-			}
-		?>
-	</div>
-	
-	<!-- Below should remain as is on every page -->
-	<div id="footer">
-		<?php require_once('templates/layout/footer.php'); ?>
-	</div>
-	<!-- Above should remain as is on every page -->
-</body>
+		unset($_SESSION['FORM_SECRET']);
 
-</html>
+		$member = validateUser($username, $password);
+		if(!$member)
+		{
+			header("Location: login-form.php");
+			exit ;
+		}
+		else
+		{
+			session_regenerate_id();
+		
+			$_SESSION['UID'] = $member->uid;
+			$_SESSION['CLASS'] = $member->class;
+										
+			session_write_close();
+			
+			header("Location: profile.php?uid=" . $_SESSION['UID']);
+			exit ;
+		}
+	}
+	else
+	{
+		//Invalid secret key
+		echo "something you did is wrong, you are not supposed to even print this";
+	}
+}
+else
+{
+	//Secret key missing
+	echo "form data has been processed";
+}
+?>
--- registration-exec.php
+++ registration-exec.php
@@ -1,80 +1,38 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+require_once ("deps/session.inc");
+require_once ("deps/presentation.inc");
+require_once ("deps/database.inc");
 
-<html>
+$form_secret = $_POST['form_secret'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-<!-- Page dependent settings such as settitle -->
-<?php 
-	require_once("deps/session.inc");
-	require_once("deps/presentation.inc");
-	require_once("deps/database.inc");
-	$title = settitle(__REGISTRATION);
-	$thisPage = __REGISTRATION;
-?>
-
-<head>
-	<!-- Global head attributes and scripts (JQuery, etc.) -->	
-	<?php require_once('templates/head.inc') ?>
-	
-	<!-- Page-specific head attributes -->
-</head>
-
-<body>
-	<!-- Below should remain as is on every page -->
-	<div id="title">
-		<?php require_once('templates/layout/title.php'); ?>
-	</div>
-	
-	<div id="language">
-		<?php require_once('templates/layout/language.php'); ?>
-	</div>
-	
-	<div id="navigation">
-		<?php require_once('templates/layout/navigation.php'); ?>
-	</div>
-	<!-- Above should remain as is on every page -->
-	
-	<div id="content">
-		<?php
+if(isset($_SESSION['FORM_SECRET']))
+{
+	if(strcasecmp($form_secret, $_SESSION['FORM_SECRET']) === 0)
+	{
+		if(!$username || !$email || !$password)
+		{
+			echo __NOTALLDETAILS . '<br />';
+			exit ;
+		}
 		
-			$form_secret = $_POST['form_secret'];
-			$username=$_POST['username'];
-			$email=$_POST['email'];
-			$password=$_POST['password'];
-			
-			if(isset($_SESSION['FORM_SECRET']))
-			{
-				if(strcasecmp($form_secret, $_SESSION['FORM_SECRET'])===0)
-				{
-					if( !$username || !$email || !$password )
-					{
-						echo __NOTALLDETAILS.'<br />';
-						exit;
-					}
-					
-					$uid = insertUser( $username, $email, $password);
-					unset($_SESSION['FORM_SECRET']);
-					header("Location: profile.php?uid=".$uid);
-					exit;
-				}
-				else
-				{
-					//Invalid secret key
-					echo "something you did is wrong, you are not supposed to even print this";
-				}
-			}
-			else
-			{
-				//Secret key missing
-				echo "form data has been processed";
-			}
-		?>
-	</div>
-	
-	<!-- Below should remain as is on every page -->
-	<div id="footer">
-		<?php require_once('templates/layout/footer.php'); ?>
-	</div>
-	<!-- Above should remain as is on every page -->
-</body>
-
-</html>
+		unset($_SESSION['FORM_SECRET']);
+		
+		insertUser($username, $email, $password);
+		header("Location: profile.php?uid=".$_SESSION['UID']);
+		exit ;
+	}
+	else
+	{
+		//Invalid secret key
+		echo "something you did is wrong, you are not supposed to even print this";
+	}
+}
+else
+{
+	//Secret key missing
+	echo "form data has been processed";
+}
+?>
