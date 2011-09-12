950ca0d4e5828a399afd87122249eff809496dc9
Lefteris Aslanoglou <leftos@gmail.com>
2011-09-12 21:34:16 +0300

    - Changing language is now only allowed on pages that do not require any GET parameters to work, since changing language loses all of them.

--- templates/layout/language.php
+++ templates/layout/language.php
@@ -1,4 +1,6 @@
 <?php
+if ((count($_GET) == 0) || ((count($_GET) == 1) && (isset($_GET['lang'])))) {
 ?>
 	<img src="images/gr.GIF" onclick="changeLang('gr')"/>&nbsp;
 	<img src="images/eng.JPG" onclick="changeLang('en')"/>
+	<? } ?>
