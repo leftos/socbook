9b46ac0b35471700adc243f2f92113993d38fdcd
Lefteris Aslanoglou <leftos@gmail.com>
2011-09-12 21:37:15 +0300

    Language change allowed when no $_POST parameters are set, since they would be lost in a language change.
    Signed-off-by: Lefteris Aslanoglou <leftos@gmail.com>

--- templates/layout/language.php
+++ templates/layout/language.php
@@ -1,6 +1,11 @@
 <?php
-if ((count($_GET) == 0) || ((count($_GET) == 1) && (isset($_GET['lang'])))) {
+if (count($_POST) == 0)
+{
+	if ((count($_GET) == 0) || ((count($_GET) == 1) && (isset($_GET['lang'])))) {
 ?>
 	<img src="images/gr.GIF" onclick="changeLang('gr')"/>&nbsp;
 	<img src="images/eng.JPG" onclick="changeLang('en')"/>
-	<? } ?>
+	<? 
+	}
+}
+?>
