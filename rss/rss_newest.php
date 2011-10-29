a9730d1d865d4af7cd6d27855a3c16f472779882
Lefteris Aslanoglou <leftos@gmail.com>
2011-10-30 00:45:31 +0300

    Fixed RSS feed.

--- rss/rss_newest.php
+++ rss/rss_newest.php
@@ -1,25 +1,28 @@
 <? 
-	header('Content-type: text/xml');
+	header('Content-Type: application/rss+xml');
 	while(!file_exists(getcwd()."/.htroot")){chdir('..');}
 	require_once('deps/database.inc');
+	require_once('deps/presentation.inc');
 ?> 
-
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
 <title>SocBook</title>
 <description>Social Bookmarking done ...in an average way</description>
-<link>http://leftos.zapto.org:8080</link>
-
+<link>http://socbook.dyndns.org:8088</link>
+<atom:link href="http://socbook.dyndns.org:8088/rss/rss_newest.php" rel="self" type="application/rss+xml" />
 <?
 $result = populateIndex('datecreated');
 while($row = $result->fetch_object()){
 ?>
 
 <item>
-<title><?=$row->title ?></title>
-<link>http://leftos.zapto.org:8080/viewbookmark.php?bid=<?=$row->bid ?></link>
-<description><?=$row->comment ?></description>
-<pubDate><?=$row->info ?></pubDate>
+	<title><? echo htmlspecialchars($row->title); ?></title>
+	<link>http://socbook.dyndns.org:8088/viewbookmark.php?bid=<?=$row->bid ?></link>
+	<description><?=$row->comment ?></description>
+	<pubDate><? 
+				$datetime=$row->info;
+				echo RFC2822($datetime);
+	?></pubDate>
 </item>
 
 <?
